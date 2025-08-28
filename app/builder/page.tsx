6447a23b67241333da2edd866ada114c3f18ca5b
Austin Oligario <austinoligario@gmail.com>
2025-08-28 14:38:05 -0400

    wip(builder): additional mocking

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -2,7 +2,7 @@ import { Builder } from "@/components/builder";
 
 export default async function BuilderPage() {
 	return (
-		<div className="p-4 m-4 mx-auto max-w-2xl border rounded bg-slate-100">
+		<div className="p-4 m-4 mx-auto max-w-2xl">
 			<h1 className="mb-4">Builder Page</h1>
 
 			<Builder />
